295677fd15296646dfbd36451f251c9574ba5aa5
leandersonnascimentosantos8658 <129195205+leandersonnascimentosantos8658@users.noreply.github.com>
2024-08-06 08:17:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const perguntas = 
+    {
+        enunciado: "Como você acredita que podemos combater o racismo na sociedade?",
+        alternativas: [
+            "Participando de campanhas de conscientização e educação sobre igualdade racial.",
+            "Denunciando atos de discriminação e apoiando políticas públicas inclusivas."
+        ]
+    }

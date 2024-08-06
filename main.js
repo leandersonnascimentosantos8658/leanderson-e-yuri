7369128edf2cbbe2288f077a346e13e68d30ff9a
leandersonnascimentosantos8658 <129195205+leandersonnascimentosantos8658@users.noreply.github.com>
2024-08-06 08:11:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const caixaPricipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultados");
+const textoResultado = document.querySelector(".texto-resultado");
